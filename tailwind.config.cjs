190273c0de2a478bb48b434c21e40655716c25ee
Adam Levoy <ajlevoy@gmail.com>
2023-12-14 09:46:09 -0800

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00adb5",
+        accent: "#ac7dd2",
+      },
+    },
+  },
+  plugins: [],
+};
