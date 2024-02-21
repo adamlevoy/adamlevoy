2e69655c8483dd14f68d8a907afe4931ed7d498f
Adam Levoy <ajlevoy@gmail.com>
2024-02-21 11:19:37 -0800

    debugging mobile nav

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {},
   daisyui: {
-    themes: ["cupcake", "retro", "dracula"],
+    themes: ["forest", "halloween", "cupcake", "retro", "dracula"],
   },
   plugins: [require("daisyui")],
 };
